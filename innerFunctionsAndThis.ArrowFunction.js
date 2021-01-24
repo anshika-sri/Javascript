f2e48b636e0997cc34fc60b092623b5af1ca7314
anshika-sri <35023358+anshika-sri@users.noreply.github.com>
2021-01-24 13:46:12 +0530

    Create innerFunctionsAndThis.ArrowFunction.js

---
+++ innerFunctionsAndThis.ArrowFunction.js
@@ -0,0 +1,20 @@
+var f = function(a) {
+
+this.a = '1';
+
+var obj = {
+    1:'1'
+};
+
+var self = this;
+
+  var f1 = (b) => {
+    console.log('inner', this.a, a+b);
+  }
+
+return f1;
+
+}
+
+//OUTPUT
+inner 1 5
