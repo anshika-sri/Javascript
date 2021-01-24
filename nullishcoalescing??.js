b2514eb12cb12abccfe3989194676425ba04b026
anshika-sri <35023358+anshika-sri@users.noreply.github.com>
2021-01-25 00:08:31 +0530

    Update nullishcoalescing??.js

--- nullishcoalescing??.js
+++ nullishcoalescing??.js
@@ -5,8 +5,19 @@ person.name || 'default'; //||
 "default"
 
 
-var person = { name : '', age: 24 }
+var person = { name : '', age: 0 }
 person.name ?? 'default'; //??
+person.age ?? 24; //??
 
 //OUTPUT
 ""
+0
+
+
+var person = {  }
+person.name ?? 'default'; //??
+person.age ?? 24; //??
+
+//OUTPUT
+"default"
+24
