bb9c052b03c8a449cd64622e70d6d1f9a06e782c
anshika-sri <35023358+anshika-sri@users.noreply.github.com>
2021-04-01 03:46:01 +0530

    Create errors1.js

---
+++ LETnCONSTnVAR/errors1.js
@@ -0,0 +1,19 @@
+var a = 10;
+
+var a = 'b'
+undefined
+console.log(a)
+b
+
+var a = {1: 1, 1: '11' }
+a[1]
+"11"
+
+
+let a = 'A';
+Uncaught SyntaxError: Identifier 'a' has already been declared
+
+let b = 'a'
+var b = 'bb'
+Uncaught SyntaxError: Identifier 'b' has already been declared
+    at <anonymous>:1:1
